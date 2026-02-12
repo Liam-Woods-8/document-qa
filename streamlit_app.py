defd05ec1ea3471094220c6ea6e5cb3accbc1939
Liam Woods <143724842+Liam-Woods-8@users.noreply.github.com>
2026-02-12 20:29:39 +0000

    setting up lab4.py multipage and requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit as st
 lab1_page = st.Page("lab1.py", title="lab 1")
 lab2_page = st.Page("lab2.py", title="lab 2")
 lab3_page = st.Page("lab3.py", title="lab 3")
+lab4_page = st.Page("lab4.py", title="lab 4")
 
-pg = st.navigation([lab3_page, lab2_page, lab1_page])
+pg = st.navigation([lab4_page, lab3_page, lab2_page, lab1_page])
 
 pg.run()
