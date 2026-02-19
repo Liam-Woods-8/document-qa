556afeb682dceb00fe0056cd5415a514b1a21e8e
Liam Woods <143724842+Liam-Woods-8@users.noreply.github.com>
2026-02-19 22:00:31 +0000

    nav fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ lab1_page = st.Page("lab1.py", title="lab 1")
 lab2_page = st.Page("lab2.py", title="lab 2")
 lab3_page = st.Page("lab3.py", title="lab 3")
 lab4_page = st.Page("lab4.py", title="lab 4")
+lab5_page = st.Page("lab5.py", title="lab 5")
 
-pg = st.navigation([lab4_page, lab3_page, lab2_page, lab1_page])
+pg = st.navigation([lab5_page,lab4_page, lab3_page, lab2_page, lab1_page])
 
 pg.run()
